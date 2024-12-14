dafa03c7bff3e7676bff90fe9b213b70037dbb67
PoivretJulien <149439054+PoivretJulien@users.noreply.github.com>
2024-12-14 23:11:36 +0100

    Update lib.rs

--- lib.rs
+++ lib.rs
@@ -1,7 +1,6 @@
 // ************************************************************************
 // ******* First scratch of a basic computational component class *********
 // ************************************************************************
-#[allow(dead_code)]
 mod rust_3d {
     // Implementation of a Point3d structure
     // bound to Vector3d structure
