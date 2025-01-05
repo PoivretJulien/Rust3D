74994cc68fdb04031a925c2073dde9d8d1adb0a0
PoivretJulien <149439054+PoivretJulien@users.noreply.github.com>
2025-01-05 17:42:32 +0100

    Add files via upload

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+// load module.
+mod rust3d;
+mod render_tools;
+mod virtual_space;
+mod models_3d;
+use render_tools::rendering_object::Mesh;
+use rust3d::geometry::{CPlane,Point3d,Vector3d};
+use virtual_space::{Virtual_space,DisplayPipeLine};
+//Start the main thread.
+fn main(){
+    
+}
+
