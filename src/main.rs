d9dc6945cfa2e0e17913b4460f13e3c46274a100
PoivretJulien <149439054+PoivretJulien@users.noreply.github.com>
2025-02-05 15:24:59 +0100

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -28,7 +28,7 @@ fn main() {
     // Code some Geometry manipulation there.
     // Start by importing an object. (a torus test mesh in a .obj file.)
     let mut obj_mesh = Mesh::new();
-    match Mesh::import_obj_with_normals("./geometry/dk.obj") {
+    match Mesh::import_obj_with_normals("./geometry/ghost_b.obj") {
         Ok(obj) => {
             obj_mesh = obj;
         }
